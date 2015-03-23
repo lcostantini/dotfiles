774875b61639a943e70fef3912b9db22e337cb2c
lcostantini <costantinileandro1@gmail.com>
2015-03-23 18:55:40 -0300

    New aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,9 @@
 alias reload!='. ~/.zshrc'
+
+# My personal alias
+alias be='bundle exec'
+
+alias si='sudo apt-get install'
+alias sr='sudo apt-get remove --purge'
+
+alias esc='xmodmap ~/.xmodmap'

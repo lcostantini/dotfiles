53b37d2db2e93ce5546a4d7e595a47c214db3859
lcostantini <costantinileandro1@gmail.com>
2015-05-18 19:09:32 -0300

    Add more personal aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,5 +5,8 @@ alias be='bundle exec'
 
 alias si='sudo apt-get install'
 alias sr='sudo apt-get remove --purge'
+alias sag='sudo apt-get'
+alias uu='sudo apt-get update && sudo apt-get upgrade'
+alias aa='sudo apt-get autoclean && sudo apt-get autoremove'
 
 alias esc='xmodmap ~/.xmodmap'

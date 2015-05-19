c00202a27a4e3fd2400411eb42d3e2dd941b6f50
lcostantini <costantinileandro1@gmail.com>
2015-05-19 10:02:31 -0300

    Add alias for run redis.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,6 @@ alias reload!='. ~/.zshrc'
 
 # My personal alias
 alias be='bundle exec'
-
 alias si='sudo apt-get install'
 alias sr='sudo apt-get remove --purge'
 alias sag='sudo apt-get'
@@ -10,3 +9,4 @@ alias uu='sudo apt-get update && sudo apt-get upgrade'
 alias aa='sudo apt-get autoclean && sudo apt-get autoremove'
 
 alias esc='xmodmap ~/.xmodmap'
+alias redis='~/run-redis.sh'

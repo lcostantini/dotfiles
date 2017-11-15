fe4edc3bc01148b832b62e96ed6d6693d2d5631f
lcostantini <costantinileandro1@gmail.com>
2017-11-15 15:27:08 -0300

    Add nats script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,9 +5,11 @@ alias si='sudo apt-get install'
 alias sr='sudo apt-get remove --purge'
 alias sag='sudo apt-get'
 alias uu='sudo apt-get update && sudo apt-get upgrade'
+alias up='sudo apt-get update'
 alias aa='sudo apt-get autoclean && sudo apt-get autoremove'
 
 # Alias for work
 alias be='bundle exec'
 alias esc='xmodmap ~/.dotfiles/script/.xmodmap'
 alias redis='~/.dotfiles/script/run-redis.sh'
+alias nats='~/.dotfiles/script/run-nats.sh'
